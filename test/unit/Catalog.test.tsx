8b0478a0a6235c98de6309e3cb5bc6418f8edef4
GalibIbr <galibiavi4@gmail.com>
2024-07-03 23:29:40 +0300

    add unit test for Catalog.tsx / create mocks

--- test/unit/Catalog.test.tsx
+++ test/unit/Catalog.test.tsx
@@ -1,12 +1,94 @@
-import "@testing-library/jest-dom";
+import { it, expect } from "@jest/globals";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import { Provider } from "react-redux";
+import { initStore } from "../../src/client/store";
+import { Application } from "../../src/client/Application";
+import React from "react";
 
-import * as React from "react";
-import { render, screen } from "@testing-library/react";
+import { mockApi, mockCart, productsData } from "../mocks/mocks";
 
-import { it } from "@jest/globals";
+const getCheckoutRes = () => {
+  return { id: 1 };
+};
+const store = initStore(mockApi, mockCart);
 
-it("renders without crashing", () => {
-  render(<div>hello</div>);
+describe("Catalog", () => {
+  const store = initStore(mockApi, mockCart);
+  const application = (
+    <MemoryRouter initialEntries={["/catalog"]} initialIndex={0}>
+      <Provider store={store}>
+        <Application />
+      </Provider>
+    </MemoryRouter>
+  );
+  const { container } = render(application);
+  const productItems = container.getElementsByClassName("ProductItem");
 
-  expect(screen.getByText("hello")).toBeInTheDocument();
+  it("должны отображаться товары из списка, полученного с сервера", async () => {
+    for (let i = 0; i < productItems.length; i++) {
+      const productItem = productItems[i];
+      const titleElement = productItem.querySelector(".ProductItem-Name");
+      const titleText = titleElement?.textContent;
+      const titleFound = productsData.find((item) => item.name === titleText);
+      expect(titleElement).toBeTruthy();
+      expect(titleFound).toBeTruthy();
+    }
+  });
+
+  it("для каждого товара в каталоге отображается название товара", async () => {
+    for (let i = 0; i < productItems.length; i++) {
+      const div = productItems[i];
+      const titleElement = div.querySelector(".ProductItem-Name");
+      expect(titleElement).toBeTruthy();
+      expect(titleElement?.textContent).toBeTruthy();
+    }
+  });
+
+  it("должен отображаться цена для каждого товара в каталоге", async () => {
+    for (let i = 0; i < productItems.length; i++) {
+      const div = productItems[i];
+      const priceElement = div.querySelector(".ProductItem-Price");
+      expect(priceElement).toBeTruthy();
+      expect(priceElement?.textContent).toBeTruthy();
+    }
+  });
+
+  it("каждый товар в каталоге имеет ссылку на подробности", async () => {
+    for (let i = 0; i < productItems.length; i++) {
+      const div = productItems[i];
+      const link = div.querySelector(".ProductItem-DetailsLink");
+      expect(link).toBeTruthy();
+      expect(link?.textContent).toBeTruthy();
+    }
+  });
+
+  it('на странице с товарами отображаются: название, цена, цвет, материал и кнопка "в корзину"', async () => {
+    const productItem = productsData[1];
+    const store = initStore(mockApi, mockCart);
+    const application = (
+      <MemoryRouter initialEntries={[`/catalog/${productItem.id}`]} initialIndex={0}>
+        <Provider store={store}>
+          <Application />
+        </Provider>
+      </MemoryRouter>
+    );
+    const { container, findByText } = render(application);
+    const name = await findByText(productItem.name);
+    expect(name.textContent).toBeTruthy();
+    const price = await findByText(`$${productItem.price}`);
+    expect(price.textContent).toBeTruthy();
+    const description = container.querySelector(".ProductDetails-Description");
+    expect(description).toBeTruthy();
+    expect(description?.textContent).toBeTruthy();
+    const color = container.querySelector(".ProductDetails-Color");
+    expect(color).toBeTruthy();
+    expect(color?.textContent).toBeTruthy();
+    const material = container.querySelector(".ProductDetails-Material");
+    expect(material).toBeTruthy();
+    expect(material?.textContent).toBeTruthy();
+    const button = container.querySelector(".ProductDetails-AddToCart");
+    expect(button).toBeTruthy();
+    expect(button?.textContent).toBeTruthy();
+  });
 });
