b50ae54cf5fce6319332c28803e3de7ba9a97f15
GalibIbr <galibiavi4@gmail.com>
2024-07-05 12:53:25 +0300

    update catalog tests

--- test/testplane/catalog.testplane.ts
+++ test/testplane/catalog.testplane.ts
@@ -2,21 +2,19 @@ import { localURL } from "./helper/localUrl";
 
 describe('Каталог', () => {
 
-   it('В каталоге для каждого товара отображаются название, цена, ссылка', async ({ browser }) => {
+   it('В каталоге для каждого товара отображаются изображение, название, цена, ссылка', async ({ browser }) => {
       await browser.url(localURL('/catalog'));
-      const productItems = await browser.$$('.ProductItem');
-      for (let i = 0; i < productItems.length; i++) {
-         const productItem = productItems[i];
-         const productNameText = await productItem.$('.ProductItem-Name').getText();
-         const productPriceText = await productItem.$('.ProductItem-Price').getText();
-         const productDetailsLinkText = await productItem.$('.ProductItem-DetailsLink').getText();
-         const productDetailsLinkHref = await productItem.$('.ProductItem-DetailsLink').getAttribute('href');
-         expect(productNameText).toBeTruthy();
-         expect(productPriceText).toBeTruthy();
-         expect(productPriceText).toMatch(new RegExp(/\$[0-9]+.*/i));
-         expect(productDetailsLinkText).toBeTruthy();
-         expect(productDetailsLinkHref).toBeTruthy();
-      }
+
+      const images = await browser.$$('.Image');
+      const names = await browser.$$('.ProductItem-Name');
+      const prices = await browser.$$('.ProductItem-Price');
+      const links = await browser.$$('.ProductItem-DetailsLink');
+
+      await Promise.all(images.map((image) => expect(image).toBeDisplayed()));
+      await Promise.all(names.map((name) => expect(name).toBeDisplayed()));
+      await Promise.all(prices.map((price) => expect(price).toBeDisplayed()));
+      await Promise.all(links.map((link) => expect(link).toBeDisplayed()));
+      await Promise.all(links.map((link) => expect(link.getAttribute('href')).not.toBeUndefined()));
    });
 
    it('товары из каталога открываются корректно', async ({ browser }) => {
@@ -34,4 +32,24 @@ describe('Каталог', () => {
          await browser.url(localURL('/catalog'));
       }
    });
+
+   it('товар успешно добавляется в корзину', async ({ browser }) => {
+      await browser.url(localURL('/catalog'));
+
+      const detailsLink = await browser.$('.ProductItem-DetailsLink');
+      await detailsLink.click();
+
+      const addToCart = await browser.$('.ProductDetails-AddToCart');
+      await addToCart.waitForDisplayed();
+      await addToCart.click();
+      await addToCart.click();
+
+      await browser.url(localURL('/cart'));
+
+      const cartInHeader = await browser.$('[href="/hw/store/cart"]');
+      const table = await browser.$('.Cart-Table');
+
+      expect(cartInHeader).toHaveText('Cart (1)');
+      expect(table).toBeDisabled();
+   })
 })
