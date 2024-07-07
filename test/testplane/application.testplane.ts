96866692579a95ce7042526e73995c53d0ede331
GalibIbr <galibiavi4@gmail.com>
2024-07-07 11:28:03 +0300

    update integration tests

--- test/testplane/application.testplane.ts
+++ test/testplane/application.testplane.ts
@@ -1,52 +1,61 @@
 import { localURL } from "./helper/localUrl";
 
-// describe('Страницы главная, условия доставки, контакты должны иметь статическое содержимое', () => {
+describe('Страницы главная, условия доставки, контакты должны иметь статическое содержимое', () => {
 
-//     afterEach(async ({ browser }) => {
-//         await browser.assertView('plain', 'body')
-//     })
+    afterEach(async ({ browser }) => {
+        await browser.assertView('plain', 'body')
+    })
 
-//     it('Страница главная имеет статическое содержимое', async ({ browser }) => {
-//         await browser.url(localURL('/'))
-//     })
+    it('Страница главная имеет статическое содержимое', async ({ browser }) => {
+        await browser.url(localURL('/'))
+    })
 
-//     it('Страница условия доставки имеет статическое содержимое', async ({ browser }) => {
-//         await browser.url(localURL('/delivery'))
-//     })
+    it('Страница условия доставки имеет статическое содержимое', async ({ browser }) => {
+        await browser.url(localURL('/delivery'))
+    })
 
-//     it('Страница контакты имеет статическое содержимое', async ({ browser }) => {
-//         await browser.url(localURL('/contacts'))
-//     })
+    it('Страница контакты имеет статическое содержимое', async ({ browser }) => {
+        await browser.url(localURL('/contacts'))
+    })
 
-// })
+})
 
 describe('Общие требования', () => {
-    // it('вёрстка должна адаптироваться под ширину экрана', async ({ browser }) => {
-    //     await browser.url(localURL('/'))
-    //     await browser.assertView('desktop', 'body')
-    //     await browser.setWindowSize(575, 1080)
-    //     await browser.assertView('mobile', 'body')
-    // })
+    it('вёрстка должна адаптироваться под ширину экрана', async ({ browser }) => {
+        await browser.url(localURL('/'))
+        await browser.assertView('desktop', 'body')
+        await browser.setWindowSize(575, 1080)
+        await browser.assertView('mobile', 'body')
+    })
 
-    // it('в шапке отображаются ссылки на страницы магазина, а также ссылка на корзину', async ({ browser }) => {
-    //     await browser.url(localURL('/'))
-    //     await browser.assertView('header', 'body')
-    // })
+    it('в шапке отображаются ссылки на страницы магазина, а также ссылка на корзину', async ({ browser }) => {
+        await browser.url(localURL('/'))
+        await browser.assertView('header', 'body')
+    })
 
-    // it('на ширине меньше 576px навигационное меню должно скрываться за "гамбургер"', async ({ browser }) => {
-    //     await browser.url(localURL('/'))
-    //     await browser.setWindowSize(575, 1080)
-    //     await browser.assertView('burger', 'body')
-    // })
-
-    it('при выборе элемента из меню "гамбургера", меню должно открываться и при при повторном выборе - закрываться', async ({ browser }) => {
+    it('на ширине меньше 576px навигационное меню должно скрываться за "гамбургер"', async ({ browser }) => {
         await browser.url(localURL('/'))
         await browser.setWindowSize(575, 1080)
-        const burger = await browser.$('.Application-Toggler')
-        await burger.click()
-        await browser.assertView('burger open', 'body')
-        await burger.click()
-        await browser.assertView('burger close', 'body')
+        await browser.assertView('burger', 'body')
     })
+
+    it('при выборе элемента из меню "гамбургера", меню должно закрываться', async ({ browser }) => {
+        await browser.url(localURL("/"));
+        await browser.setWindowSize(575, 800);
+
+        const hamburgerButton = await browser.$(".navbar-toggler");
+        expect(hamburgerButton).toExist();
+
+        await hamburgerButton.click();
+
+        const navMenu = await browser.$(".navbar-collapse");
+        expect(await navMenu.isDisplayed()).toBe(true);
+
+        const menuItem = await navMenu.$("a[href='/hw/store/catalog']");
+        await menuItem.click();
+
+        expect(await navMenu.isDisplayed()).toBe(false);
+    });
+
 
 })
--- test/testplane/product.testplane.ts
+++ test/testplane/product.testplane.ts
@@ -2,37 +2,37 @@ import { localURL } from "./helper/localUrl"
 
 describe('Продукт', () => {
 
-   // beforeEach(async ({ browser }) => {
-   //    await browser.url(localURL('/hw/store/catalog/1'))
-   // })
+   beforeEach(async ({ browser }) => {
+      await browser.url(localURL('/hw/store/catalog/1'))
+   })
 
-   // it("должен отображать название", async ({ browser }) => {
-   //    const name = await browser.$('.ProductDetails-Name');
-   //    await name.isDisplayed()
-   //    expect(name).toBeTruthy()
-   // })
+   it("должен отображать название", async ({ browser }) => {
+      const name = await browser.$('.ProductDetails-Name');
+      await name.isDisplayed()
+      expect(name).toBeTruthy()
+   })
 
-   // it('должен отображать цену', async ({ browser }) => {
-   //    const price = await browser.$('.ProductDetails-Price');
-   //    await price.isDisplayed()
-   //    expect(price).toBeTruthy()
-   // })
+   it('должен отображать цену', async ({ browser }) => {
+      const price = await browser.$('.ProductDetails-Price');
+      await price.isDisplayed()
+      expect(price).toBeTruthy()
+   })
 
-   // it('должен отображать описание', async ({ browser }) => {
-   //    const description = await browser.$('.ProductDetails-Description');
-   //    await description.isDisplayed()
-   //    expect(description).toBeTruthy()
-   // })
+   it('должен отображать описание', async ({ browser }) => {
+      const description = await browser.$('.ProductDetails-Description');
+      await description.isDisplayed()
+      expect(description).toBeTruthy()
+   })
 
-   // it('должен отображать цвет', async ({ browser }) => {
-   //    const color = await browser.$('.ProductDetails-Color');
-   //    await color.isDisplayed()
-   //    expect(color).toBeTruthy()
-   // })
+   it('должен отображать цвет', async ({ browser }) => {
+      const color = await browser.$('.ProductDetails-Color');
+      await color.isDisplayed()
+      expect(color).toBeTruthy()
+   })
 
-   // it('должен отображать материал', async ({ browser }) => {
-   //    const material = await browser.$('.ProductDetails-Material');
-   //    await material.isDisplayed()
-   //    expect(material).toBeTruthy()
-   // })
+   it('должен отображать материал', async ({ browser }) => {
+      const material = await browser.$('.ProductDetails-Material');
+      await material.isDisplayed()
+      expect(material).toBeTruthy()
+   })
 })
